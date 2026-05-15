54c91cf83b2f08a21783404aa313b2c3fedbc11f
Lương Linh Khôi <luongkhoi3456@gmail.com>
2026-05-15 12:55:17 +0700

    fix: fix Level Selector behaviour

--- Assets/Scripts/UI/LevelButton.cs
+++ Assets/Scripts/UI/LevelButton.cs
@@ -1,22 +1,39 @@
+using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelButton : MonoBehaviour
 {
     [SerializeField] private TMP_Text label;
+    [SerializeField] private Sprite normalSprite;
+    [SerializeField] private Sprite selectedSprite;
 
     private int level;
-    private LevelManager levelManager;
+    private Action<LevelButton,int> onSelected;
+    private Button button;
 
-    public void Setup(int level, LevelManager manager)
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+
+    public void Setup(int level, Action<LevelButton,int> onSelected)
     {
         this.level = level;
-        this.levelManager = manager;
+        this.onSelected = onSelected;
         label.text = level.ToString();
+        SetSelected(false);
     }
 
     public void OnClick()
     {
-        levelManager.StartLevel(level);
+        onSelected?.Invoke(this, level);
+    }
+
+    public void SetSelected(bool selected)
+    {
+        if (button == null || button.image == null) return;
+        button.image.sprite = selected && selectedSprite != null ? selectedSprite : normalSprite;
     }
 }
--- Assets/Scripts/UI/LevelSelectorUI.cs
+++ Assets/Scripts/UI/LevelSelectorUI.cs
@@ -5,14 +5,34 @@ public class LevelSelectorUI : MonoBehaviour
     [SerializeField] private Transform container;
     [SerializeField] private LevelButton buttonPrefab;
     [SerializeField] private LevelManager levelManager;
-    private int totalLevels = 10;
+    [SerializeField] private int totalLevels = 10;
+
+    private LevelButton selectedButton;
+    private int selectedLevel = -1;
 
     private void Start()
     {
         for (int i = 1; i <= totalLevels; i++)
         {
             var btn = Instantiate(buttonPrefab, container);
-            btn.Setup(i, levelManager);
+            btn.Setup(i, (b, lvl) => OnButtonSelected(b, lvl));
         }
+    }
+
+    private void OnButtonSelected(LevelButton button, int level)
+    {
+        if (selectedButton != null)
+            selectedButton.SetSelected(false);
+
+        selectedButton = button;
+        selectedLevel = level;
+        selectedButton.SetSelected(true);
+    }
+
+    // Wire this to the Play button's onClick in the inspector
+    public void OnPlayButton()
+    {
+        if (selectedLevel <= 0) return;
+        levelManager.StartLevel(selectedLevel);
     }
 }
