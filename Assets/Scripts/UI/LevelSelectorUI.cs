0375f9c7e7c36c84b5d9f4afa0099c360a903aa8
Lương Linh Khôi <luongkhoi3456@gmail.com>
2026-05-15 12:37:59 +0700

    chore: reconstruct files

--- Assets/Scripts/UI/LevelSelectorUI.cs
+++ Assets/Scripts/UI/LevelSelectorUI.cs
@@ -5,10 +5,11 @@ public class LevelSelectorUI : MonoBehaviour
     [SerializeField] private Transform container;
     [SerializeField] private LevelButton buttonPrefab;
     [SerializeField] private LevelManager levelManager;
+    private int totalLevels = 10;
 
     private void Start()
     {
-        for (int i = 1; i <= 10; i++)
+        for (int i = 1; i <= totalLevels; i++)
         {
             var btn = Instantiate(buttonPrefab, container);
             btn.Setup(i, levelManager);
